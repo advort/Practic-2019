1a86ef401cee0d7864097558d17254c8a6bc0ac0
advort <belavin24@gmail.com>
2019-07-15 17:46:23 +0300

    Addition alignment column

---
+++ Columns alignment/exportDataGrid.js
@@ -0,0 +1,80 @@
+function exportDataGrid(options) {
+    if(options === undefined) return;
+
+    let { customizeCell, component, worksheet, topLeftCell = { row: 1, column: 1 }, excelFilterEnabled } = options;
+
+    worksheet.properties.outlineProperties = {
+        summaryBelow: false,
+        summaryRight: false
+    };
+
+    let result = {
+        from: { row: topLeftCell.row, column: topLeftCell.column },
+        to: { row: topLeftCell.row, column: topLeftCell.column }
+    };
+
+    let dataProvider = component.getDataProvider();
+
+    return new Promise((resolve) => {
+        dataProvider.ready().done(() => {
+            let columns = dataProvider.getColumns();
+            let headerRowCount = dataProvider.getHeaderRowCount();
+            let dataRowsCount = dataProvider.getRowsCount();
+
+            for(let rowIndex = 0; rowIndex < dataRowsCount; rowIndex++) {
+                const row = worksheet.getRow(result.from.row + rowIndex);
+                _exportRow(rowIndex, columns.length, row, result.from.column, dataProvider, customizeCell);
+
+                if(rowIndex >= headerRowCount) {
+                    row.outlineLevel = dataProvider.getGroupLevel(rowIndex);
+                }
+                if(rowIndex >= 1) {
+                    result.to.row++;
+                }
+            }
+
+            result.to.column += columns.length > 0 ? columns.length - 1 : 0;
+
+            if(excelFilterEnabled === true) {
+                if(dataRowsCount > 0) worksheet.autoFilter = result;
+                worksheet.views = [{ state: 'frozen', ySplit: result.from.row + dataProvider.getFrozenArea().y - 1 }];
+            }
+
+            resolve(result);
+        });
+    });
+}
+
+function _exportRow(rowIndex, cellCount, row, startColumnIndex, dataProvider, customizeCell) {
+    for(let cellIndex = 0; cellIndex < cellCount; cellIndex++) {
+        const cellData = dataProvider.getCellData(rowIndex, cellIndex, true);
+        const cell = row.getCell(startColumnIndex + cellIndex);
+        //
+        switch(cellData.cellSourceData.column.alignment){
+            case 'right':
+              cell.alignment = { horizontal: 'right' };
+              break;
+            case 'left': 
+              cell.alignment = { horizontal: 'left' };
+              break;
+            case 'center': 
+              cell.alignment = { horizontal: 'center' };
+              break;
+        }
+        //
+        cell.value = cellData.value;
+
+        if(customizeCell !== undefined) {
+            customizeCell({
+                cell: cell,
+                gridCell: {
+                    column: cellData.cellSourceData.column,
+                    rowType: cellData.cellSourceData.rowType,
+                    data: cellData.cellSourceData.data,
+                    value: cellData.value,
+                    groupIndex: cellData.cellSourceData.groupIndex
+                }
+            });
+        }
+    }
+}
