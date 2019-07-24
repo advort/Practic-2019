ea7dde764c887830c5b8978060a7c9f49961be5c
advort <belavin24@gmail.com>
2019-07-24 14:22:12 +0300

    Addition showBorders, showColumnsHeaders and showColumnLines

--- showBorders showColumnLines/exportDataGrid.js
+++ showBorders showColumnLines/exportDataGrid.js
@@ -20,19 +20,104 @@ function exportDataGrid(options) {
             let columns = dataProvider.getColumns();
             let headerRowCount = dataProvider.getHeaderRowCount();
             let dataRowsCount = dataProvider.getRowsCount();
-
+            for(let i=0;i<columns.length;i++){
+                let cell;
+                let coll=worksheet.getColumn(result.from.column+i);
+                switch(i){
+                  case 0:coll.width=10;
+                    break;
+                  case 1:coll.width=30;
+                    break;
+                  case 2:coll.width=30;
+                    break;  
+                  case 3:coll.width=25;
+                    break;
+                }
+            }
             for(let rowIndex = 0; rowIndex < dataRowsCount; rowIndex++) {
                 const row = worksheet.getRow(result.from.row + rowIndex);
                 _exportRow(rowIndex, columns.length, row, result.from.column, dataProvider, customizeCell);
 
                 //
-                if(rowIndex<headerRowCount){
-                    for(let cellIndex = 0; cellIndex < columns.length; cellIndex++) {
+                let cell;
+                for(let cellIndex = 0; cellIndex < columns.length; cellIndex++) {
+                    cell = worksheet.getCell(result.from.row + rowIndex,result.from.column + cellIndex);//3
+                    if(rowIndex<headerRowCount){
                       if(dataProvider._options.columns[rowIndex][cellIndex].colspan>1||dataProvider._options.columns[rowIndex][cellIndex].rowspan>1){
                         worksheet.mergeCells(rowIndex+result.from.row,cellIndex+result.from.column,rowIndex+result.from.row+dataProvider._options.columns[rowIndex][cellIndex].rowspan-1,cellIndex+dataProvider._options.columns[rowIndex][cellIndex].colspan+result.from.column-1);
+                      }//1
+                      if(dataProvider._exportController.component._options.showColumnLines===false){
+                        cell.border=cell.border={top: {style:'thin', color: {argb:'FF666666'}}, bottom:{style:'thin', color:{argb:'FF666666'}}};
+                        if(dataProvider._exportController.component._options.showBorders===true){
+                          if(cellIndex===0){
+                            cell.border={
+                              top: {style:'thin', color: {argb:'FF666666'}},
+                              bottom: {style:'thin', color: {argb:'FF666666'}},
+                              left: {style:'thin', color: {argb:'FF666666'}}
+                            };
+                          }
+                          if(cellIndex===columns.length-1){
+                            cell.border = {
+                              top: {style:'thin', color: {argb:'FF666666'}},
+                              bottom: {style:'thin', color: {argb:'FF666666'}},
+                              right: {style:'thin', color: {argb:'FF666666'}}
+                            };
+                          }
+                        }
+                      } else {
+                        cell.border = {
+                          top: {style:'thin', color: {argb:'FF666666'}},
+                          bottom: {style:'thin', color: {argb:'FF666666'}},
+                          right: {style:'thin', color: {argb:'FF666666'}},
+                          left: {style:'thin', color: {argb:'FF666666'}}
+                        }
+                        if(dataProvider._exportController.component._options.showBorders===false){
+                          if(cellIndex===0){
+                            cell.border={
+                              top: {style:'thin', color: {argb:'FF666666'}},
+                              bottom: {style:'thin', color: {argb:'FF666666'}},
+                              right: {style:'thin', color: {argb:'FF666666'}}
+                            };
+                          }
+                          if(cellIndex===columns.length-1){
+                            cell.border = {
+                              top: {style:'thin', color: {argb:'FF666666'}},
+                              bottom: {style:'thin', color: {argb:'FF666666'}},
+                              left: {style:'thin', color: {argb:'FF666666'}}
+                            };
+                          }
+                        }
+                      }
+                      //1
+                    } else {
+                      if(dataProvider._exportController.component._options.showColumnLines===false && dataProvider._exportController.component._options.showBorders===true){
+                        if(cellIndex===0){
+                          if(rowIndex===0) cell.border = {top: {style:'thin', color: {argb:'FF666666'}}, left:{style:'thin', color:{argb:'FF666666'}}};
+                          else if(rowIndex===dataProvider.getRowsCount()-1) cell.border = {bottom: {style:'thin', color: {argb:'FF666666'}}, left:{style:'thin', color:{argb:'FF666666'}}};
+                          else cell.border = {left: {style:'thin', color: {argb:'FF666666'}}};
+                        }
+                        if(cellIndex===columns.length-1){
+                          if(rowIndex===0) cell.border = {top: {style:'thin', color: {argb:'FF666666'}}, right:{style:'thin', color:{argb:'FF666666'}}};
+                          else if(rowIndex===dataProvider.getRowsCount()-1) cell.border = {bottom: {style:'thin', color: {argb:'FF666666'}}, right:{style:'thin', color:{argb:'FF666666'}}};
+                          else cell.border = {right: {style:'thin', color: {argb:'FF666666'}}};
+                        }
+                        if(cellIndex>0 && cellIndex<columns.length-1 && rowIndex===headerRowCount) cell.border = {top: {style:'thin', color: {argb:'FF666666'}}};
+                        if(cellIndex>0 && cellIndex<columns.length-1 && rowIndex===dataProvider.getRowsCount()-1) cell.border = {bottom: {style:'thin', color: {argb:'FF666666'}}};
+                      }
+                      if(dataProvider._exportController.component._options.showColumnLines===false && dataProvider._exportController.component._options.showBorders===false && dataProvider._exportController.component._options.showColumnHeaders===false && rowIndex===headerRowCount) cell.border = {top: {style:'thin', color: {argb:'FF666666'}}};
+                      if(dataProvider._exportController.component._options.showColumnLines===true){
+                        if(dataProvider._exportController.component._options.showBorders===false && cellIndex>0 && cellIndex<columns.length-1) cell.border = {left: {style:'thin', color: {argb:'FF666666'}}, right: {style:'thin', color: {argb:'FF666666'}}};
+                        if(dataProvider._exportController.component._options.showBorders===true){
+                          if(rowIndex>headerRowCount && rowIndex<dataProvider.getRowsCount()-1) cell.border = {left: {style:'thin', color: {argb:'FF666666'}}, right: {style:'thin', color: {argb:'FF666666'}}};
+                          else{
+                            if(rowIndex===headerRowCount) cell.border = {left: {style:'thin', color: {argb:'FF666666'}}, right: {style:'thin', color: {argb:'FF666666'}}, top: {style:'thin', color: {argb:'FF666666'}}};
+                            if(rowIndex===dataProvider.getRowsCount()-1) cell.border = {left: {style:'thin', color: {argb:'FF666666'}}, right: {style:'thin', color: {argb:'FF666666'}}, bottom: {style:'thin', color: {argb:'FF666666'}}};
+                          }
+                        }
                       }
                     }
-                }
+                    //2
+                  }
                 //
                 if(rowIndex >= headerRowCount) {
                     row.outlineLevel = dataProvider.getGroupLevel(rowIndex);
@@ -40,14 +125,7 @@ function exportDataGrid(options) {
                 if(rowIndex >= 1) {
                     result.to.row++;
                 }
-            }//Можно использовать этот фрагмент, но я его закинул в основной цикл
-            /*for(let i=0; i<headerRowCount; i++){
-              for(let j=0; j<columns.length;j++){
-                if(dataProvider._options.columns[i][j].colspan>1||dataProvider._options.columns[i][j].rowspan>1){
-                  worksheet.mergeCells(i+result.from.row,j+result.from.column,i+result.from.row+dataProvider._options.columns[i][j].rowspan-1,j+dataProvider._options.columns[i][j].colspan+result.from.column-1);
-                }
-              }
-            }*/
+            }
 
             result.to.column += columns.length > 0 ? columns.length - 1 : 0;
 
@@ -65,7 +143,10 @@ function _exportRow(rowIndex, cellCount, row, startColumnIndex, dataProvider, cu
     for(let cellIndex = 0; cellIndex < cellCount; cellIndex++) {
         const cellData = dataProvider.getCellData(rowIndex, cellIndex, true);
         const cell = row.getCell(startColumnIndex + cellIndex);
-
+        if(rowIndex<dataProvider.getHeaderRowCount()){
+            cell.font=cell.font||{};
+            cell.font.bold=true;
+          }
         //Если не поставить условие, то некоторые ячейки будут пустыми.
         if(cellData.value!=='') cell.value = cellData.value;//Чтобы работало без условия, нужно использовать закомментированный фрагмент
 
